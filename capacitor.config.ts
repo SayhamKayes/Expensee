0ba872b527784b590331424e40c92e7573f10340
SayhamKayes <sayhamkayes101@gmail.com>
2026-04-28 01:22:04 +0600

    Created .aab file and keystore password

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.expensee.app',
+  appId: 'com.sayhamkayes.expensee',
   appName: 'Expensee',
   webDir: 'dist',
 };
